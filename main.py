58c9a00b43ed4c291ae2872f35599573acddb1a9
Davi Martins <davibarroso12399@gmail.com>
2026-06-14 18:03:50 -0300

    Começando a criar o banco de dados para os clientes

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import sqlite3
+
+conexao = sqlite3.connect("clientes.db")
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS clientes (
+                                            id INTERGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+                                            nome TEXT NOT NULL, 
+                                            CPF TEXT NOT NULL UNIQUE)""")
+
+conexao.commit()
+
 print("""
 (1) Cadastrar Clientes
-(2) Pedidos""")
+(2) Serviços
+(3) Pedidos""")
 
 user_input = input("Escolha uma opção: ")
 
@@ -9,6 +22,18 @@ if user_input == "1":
     cpf_cliente = input("Informe o CPF do cliente: ")
     numero_cliente = input("Informe o numero do cliente: ")
     print("Cliente cadastrado com sucesso!")
-    
-if user_input == "2":
+
+elif user_input == "2":
+    checar_nome_cliente = input("Informe o nome do cliente: ")
+#     print("""
+# =========================================================
+#                       SERVIÇOS
+# =========================================================""")
+#     print("""
+# (1) Reforma
+# (2) Bainha
+# (3) Sob Medida
+# (5) Ajuste""")
+
+elif user_input == "3":
     print("Pedidos")
